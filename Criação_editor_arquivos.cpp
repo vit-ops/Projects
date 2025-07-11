5bf208fd0379e38eb5ec7461dd0692806fd9b75f
Victor <victoraugusto146533@gmail.com>
2025-07-11 02:11:46 -0300

    Criação_editor_arquivos.cpp
    Criação, edição de arquivos, tem uma função que eu ainda n terminei.

---
+++ Criação_editor_arquivos.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void gravar(string& OqueGravar ,string& name_arquivo, int& numero){
+    ofstream Ar;
+    Ar.open(name_arquivo, ios::app);
+    if (numero!= 0){
+    Ar << OqueGravar << endl;
+    }else{
+        
+    }
+    Ar.close();
+    numero++;
+}
+void ler(string& name_arquivo){
+    string Espa = "______________________________________________________________________";
+    string linhaAr;
+    ifstream Ar;
+    Ar.open(name_arquivo);
+    cout << "itens listados no arquvo:" << endl << Espa << endl;
+    if(Ar){
+        while (getline(Ar, linhaAr)){
+            cout << linhaAr << endl;
+        }
+    }else{
+        cout << "Error" << endl;
+    }
+    cout << Espa << endl;
+    Ar.close();
+    
+}
+void Limpar_tela(){
+    system("clear");
+}
+void Limpar_Ar(string& name_arquivo){
+
+    ofstream Ar;
+    Ar.open(name_arquivo,ios::trunc);
+    Ar.close();
+}
+void ler_editar(string& OqueGravar, string& name_arquivo){
+    int num = 0;
+    string linha;
+    vector<string> lista_linhas;
+    ifstream Ar;
+    Ar.open(name_arquivo);
+    if (Ar) {
+        while (getline(Ar, linha)) {
+            lista_linhas.push_back(linha);
+        }
+    } else {
+        cout << "Error" << endl;
+    }
+    Ar.close();
+
+
+    size_t pos = OqueGravar.find('@');
+    if (pos != string::npos) {
+        int linha_num = stoi(OqueGravar.substr(0, pos)); 
+        string novo_conteudo = OqueGravar.substr(pos + 1); 
+
+
+        if (linha_num > 0 && linha_num <= lista_linhas.size()) {
+            lista_linhas[linha_num - 1] = novo_conteudo; 
+        } else {
+            cout << "Número de linha inválido!" << endl;
+            return;
+        }
+    } else {
+        cout << "Formato inválido!" << endl;
+        return;
+    }
+
+    ofstream Ar_escrita(name_arquivo);
+    for (const auto& l : lista_linhas) {
+        Ar_escrita << l << endl;
+    }
+    Ar_escrita.close();
+}
+int main(){
+    int Numero = 0;
+    string name_arquivo;
+    string OqueGravar = "";
+    cout << "Qual o nome do seu arquivo? Exemplo(Arquivo.txt) : ";
+    cin >> name_arquivo;
+    gravar(OqueGravar, name_arquivo, Numero);
+    ler(name_arquivo);
+    while (true){
+        getline(cin, OqueGravar);
+        if (OqueGravar == "sair"){
+            Limpar_tela();
+            ler(name_arquivo);
+            break;
+        }else if(OqueGravar == "clear"){
+            Limpar_Ar(name_arquivo);
+            Limpar_tela();
+            cout << "arquivo limpo!" << endl;
+            ler(name_arquivo);
+
+        }else if (OqueGravar == "help"){
+            cout << "clear : limpar o arquivo!" << endl;
+            cout << "sair : Fechar programa!" << endl;
+            cout << "Ou só digite o que deseja que seja integrado ao arquivo!" << endl;
+        }else if(isdigit(OqueGravar[0])){
+            ler_editar(OqueGravar, name_arquivo);
+            Limpar_tela();
+            ler(name_arquivo);
+        }else{
+            Limpar_tela();
+            gravar(OqueGravar, name_arquivo, Numero);
+            ler(name_arquivo);
+        }
+    }
+    
+    return 0;
+}
